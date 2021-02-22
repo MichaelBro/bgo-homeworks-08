0a58bc523903c424a5cd0dfd66dc22caf5708b30
MikhailBrovko <michael.bro@mail.ru>
2021-02-22 18:59:11 +0300

    added export/import to json

--- cmd/main.go
+++ cmd/main.go
@@ -13,44 +13,52 @@ func main() {
 			Id:        0,
 			From:      "5555 1232 2222 5555",
 			To:        "2345 7874 7437 2232",
-			Amount:    	600_00,
+			Amount:    600_00,
 			Timestamp: 1613983040,
 		},
 		{
 			Id:        1,
 			From:      "1234 4321 1234 5666",
 			To:        "2345 7874 2222 7874",
-			Amount:    	100_00,
+			Amount:    100_00,
 			Timestamp: 1613989232,
 		},
 		{
 			Id:        2,
 			From:      "2345 7874 2222 7874",
 			To:        "1234 4321 1234 2222",
-			Amount:    	1_000_00,
+			Amount:    1_000_00,
 			Timestamp: 1613989841,
 		},
 	}
 
-	err := card.ExportToCsv(transactions)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
 	fmt.Println(transactions)
 
-	transactionsByImport, err := card.ImportOfCsv("transactions.csv")
-
+	err := card.ExportToCsv(transactions)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Println(transactionsByImport)
+	transactionsByCsv, err := card.ImportOfCsv("transactions.csv")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	fmt.Println(transactionsByCsv)
 
+	err = card.ExportToJson(transactions)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	transactionsByJson, err := card.ImportFromJson("transactions.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	fmt.Println(transactionsByJson)
 }

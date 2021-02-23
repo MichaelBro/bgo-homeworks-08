d1bcd6e465b2a6dc459c1083bae7627164c50a4f
MikhailBrovko <michael.bro@mail.ru>
2021-02-23 10:15:28 +0300

    update import to github pkg

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bgo-homeworks-08/pkg/card"
 	"fmt"
+	"github.com/MichaelBro/bgo-homeworks-08/pkg/card"
 	"os"
 )
 
@@ -13,21 +13,21 @@ func main() {
 			Id:        0,
 			From:      "5555 1232 2222 5555",
 			To:        "2345 7874 7437 2232",
-			Amount:    	600_00,
+			Amount:    600_00,
 			Timestamp: 1613983040,
 		},
 		{
 			Id:        1,
 			From:      "1234 4321 1234 5666",
 			To:        "2345 7874 2222 7874",
-			Amount:    	100_00,
+			Amount:    100_00,
 			Timestamp: 1613989232,
 		},
 		{
 			Id:        2,
 			From:      "2345 7874 2222 7874",
 			To:        "1234 4321 1234 2222",
-			Amount:    	1_000_00,
+			Amount:    1_000_00,
 			Timestamp: 1613989841,
 		},
 	}
@@ -49,8 +49,5 @@ func main() {
 	}
 
 	fmt.Println(transactionsByImport)
-
-
-
 
 }
